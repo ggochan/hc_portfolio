cba7da654444e14f7740b49d341da2e21367efcf
ggochan <go950910@naver.com>
2026-02-03 01:02:23 +0900

    모달 구현 완료 & 개발 내용 작성중

--- src/data/mainData.ts
+++ src/data/mainData.ts
@@ -17,64 +17,130 @@ export const ProjectData: Project[] = [
       dev: ['Docker', 'GitHub'],
       tools: ['VS Code', 'Android Studio', 'XCode'],
     },
-    gallery: {
-      web: {
-        desktop: [
-          {
-            src: 'images/project_1/c_admin_dashboard/c_admin_dashboard_0.webp',
-            alt: '0',
-          },
-          {
-            src: '/images/project_1/c_admin_dashboard/c_admin_dashboard_1.webp',
-            alt: '1',
-          },
-          {
-            src: '/images/project_1/c_admin_dashboard/c_admin_dashboard_2.webp',
-            alt: '2',
-          },
-          {
-            src: '/images/project_1/c_admin_dashboard/c_admin_dashboard_3.webp',
-            alt: '3',
-          },
-        ],
-      },
-      userApp: {
-        android: [
-          {
-            src: '/images/project_1/c_user_android/c_user_android_0.webp',
-            alt: '0',
-          },
-          {
-            src: '/images/project_1/c_user_android/c_user_android_1.webp',
-            alt: '1',
-          },
-          {
-            src: '/images/project_1/c_user_android/c_user_android_2.webp',
-            alt: '2',
-          },
-          {
-            src: '/images/project_1/c_user_android/c_user_android_3.webp',
-            alt: '3',
-          },
-          {
-            src: '/images/project_1/c_user_android/c_user_android_4.webp',
-            alt: '4',
-          },
-        ],
-        ios: [
-          {
-            src: '/images/project_1/c_user_ios/c_user_ios_0.webp',
-            alt: '0',
-          },
-          {
-            src: '/images/project_1/c_user_ios/c_user_ios_1.webp',
-            alt: '1',
-          },
-          {
-            src: '/images/project_1/c_user_ios/c_user_ios_2.webp',
-            alt: '2',
-          },
-        ],
+    more: {
+      summary: [
+        '앱을 통해 IoT 콘센트를 결제 · 제어하고, 매장과 관리자가 이를 관리하는 구조의 플랫폼으로 <br>외주 개발로 진행되던 서비스를 인수받아 내부 요구사항에 맞게 확장 • 운영한 프로젝트 입니다. ',
+      ],
+      config: [
+        '총 5종의 사용자 앱 / 매장 관리 앱 / 관리자 대시보드 웹으로 구성',
+        '웹 Vue 2 / 앱 Android(Java), iOS(Swift) Native 기반',
+        'Element-UI 라이브러리를 이용한 UI/UX 구성',
+        'IoT 플러그와 BLE 통신을 통해 결제 후 전력 제공',
+        'Azure 클라우드 기반 Spring Boot 백엔드 서버',
+      ],
+      role: [
+        '외주 개발 산출물 및 전체 구조 파악',
+        '사용자 앱 · 매장 앱 · 관리자 웹의 프론트엔드 / 앱 개발 및 기능 확장',
+        '해외 서비스 확장을 위한 앱 · 웹 추가 개발 및 배포\n(인도네시아, 베트남, 일본, 인도 등)',
+        'FW / HW / 백엔드와 협업하며 BLE 통신 및 결제 흐름 연동',
+      ],
+      point: [
+        {
+          title: 'BLE 기반 IoT 플러그 제어',
+          content: [
+            'QR을 통해 BLE 통신을 진행하고 사용자 앱 결제 후 IoT 플러그 제어',
+          ],
+        },
+        {
+          title: '다중 사용자 구조 설계',
+          content: ['사용자 / 매장 / 관리자 역할 분리 및 기능 구성'],
+        },
+        {
+          title: '해외 PG 연동 결제 구현',
+          content: [
+            '국가별 해외 PG사 연동을 통한 결제 기능 개발 및 서비스 확장',
+          ],
+        },
+      ],
+      gallery: {
+        web: {
+          desktop: [
+            {
+              src: 'images/c_project/c_admin_dashboard/c_admin_dashboard_0.webp',
+              alt: '0',
+            },
+            {
+              src: '/images/c_project/c_admin_dashboard/c_admin_dashboard_1.webp',
+              alt: '1',
+            },
+            {
+              src: '/images/c_project/c_admin_dashboard/c_admin_dashboard_2.webp',
+              alt: '2',
+            },
+            {
+              src: '/images/c_project/c_admin_dashboard/c_admin_dashboard_3.webp',
+              alt: '3',
+            },
+          ],
+        },
+        userApp: {
+          android: [
+            {
+              src: '/images/c_project/c_user_android/c_user_android_0.webp',
+              alt: '0',
+            },
+            {
+              src: '/images/c_project/c_user_android/c_user_android_1.webp',
+              alt: '1',
+            },
+            {
+              src: '/images/c_project/c_user_android/c_user_android_2.webp',
+              alt: '2',
+            },
+            {
+              src: '/images/c_project/c_user_android/c_user_android_3.webp',
+              alt: '3',
+            },
+            {
+              src: '/images/c_project/c_user_android/c_user_android_4.webp',
+              alt: '4',
+            },
+          ],
+          ios: [
+            {
+              src: '/images/c_project/c_user_ios/c_user_ios_0.webp',
+              alt: '0',
+            },
+            {
+              src: '/images/c_project/c_user_ios/c_user_ios_1.webp',
+              alt: '1',
+            },
+            {
+              src: '/images/c_project/c_user_ios/c_user_ios_2.webp',
+              alt: '2',
+            },
+          ],
+        },
+        storeApp: {
+          android: [
+            {
+              src: '/images/c_project/c_store_android/c_store_android_0.webp',
+              alt: '0',
+            },
+            {
+              src: '/images/c_project/c_store_android/c_store_android_1.webp',
+              alt: '1',
+            },
+            {
+              src: '/images/c_project/c_store_android/c_store_android_2.webp',
+              alt: '2',
+            },
+          ],
+          ios: [
+            {
+              src: '/images/c_project/c_store_ios/c_store_ios_0.webp',
+              alt: '0',
+            },
+            {
+              src: '/images/c_project/c_store_ios/c_store_ios_1.webp',
+              alt: '1',
+            },
+            {
+              src: '/images/c_project/c_store_ios/c_store_ios_2.webp',
+              alt: '2',
+            },
+          ],
+        },
       },
     },
   },
@@ -82,7 +148,7 @@ export const ProjectData: Project[] = [
     index: 2,
     type: 2,
     status: 0,
-    title: 'HCU 검수 프로그램 개발',
+    title: '제어 유닛(HCU) 검수 윈도우 프로그램 개발',
     content: 'TCP/IP 통신 프로토콜 기반 HCU 보드 검수 프로그램 개발',
     period: { startDate: '2022-12', endDate: '2023-02' },
     task: ['Window Application'],
@@ -91,6 +157,59 @@ export const ProjectData: Project[] = [
       language: ['WPF(C#)'],
       dev: [''],
       tools: ['Visual Studio'],
+    },
+    more: {
+      summary: [
+        '제어 유닛(HCU)과 TCP/IP 통신을 통해 장비 데이터를 수집·검증하는 Windows 클라이언트 프로그램',
+      ],
+      config: [
+        'Windows 클라이언트 (C#, WPF)',
+        '설정 파일 기반 환경 구성 및 로그 저장 (txt)',
+        'Excel 연동 및 입력 / 결과 저장',
+        '제어 유닛(HCU) 연동 (TCP/IP 통신)',
+        '백엔드 서버 (추후 확장)',
+      ],
+      role: [
+        'C# WPF 기반 Windows 클라이언트 단독 개발',
+        '설정 파일(txt) 기반 환경 설정 및 관리 / 로그 저장 기능 구현',
+        'Excel 데이터(MAC, 인증서, 라이선스) 로드 및 검증 로직 구현',
+        '제어 유닛(HCU)과 TCP / IP 통신을 통한 실시간 데이터 수집 구현',
+        '검증 입력 / 결과 Excel 저장 기능 구현',
+      ],
+      point: [
+        {
+          title: 'txt 설정 파일 기반으로 동적 조회/수정/삭제 가능한 구조 설계',
+        },
+        {
+          title:
+            'Excel 데이터를 기반으로 파일 경로 인증서 · 인증키 · 라이선스 파일 비교 검증 로직 구현',
+        },
+        {
+          title:
+            '파일 비교 검증 → 장비(HCP 보드) 통신 → 오차 범위 검증의 단계적 플로우 설계',
+        },
+        {
+          title:
+            'TCP/IP 통신 중 진행 상태를 실시간으로 사용자에게 피드백하도록 UI 반응 처리',
+        },
+        {
+          title: '모든 검증 결과 및 로그를 기록하여 추적 가능성 확보',
+        },
+      ],
+      gallery: {
+        windowApp: {
+          window: [
+            {
+              src: '/images/hcu_project/hcu_0.webp',
+              alt: '0',
+            },
+            {
+              src: '/images/hcu_project/hcu_1.webp',
+              alt: '1',
+            },
+          ],
+        },
+      },
     },
   },
   {
@@ -105,16 +224,184 @@ export const ProjectData: Project[] = [
     task: ['FrontEnd', 'App Application(Android, iOS)'],
     personnel: 5,
     skills: {
-      language: ['Vue 3', 'JavaScript', 'Java', 'Swift'],
+      language: ['Vue 3', 'TypeScript', 'Java', 'Swift'],
       dev: ['Docker', 'GitHub'],
       tools: ['VS Code', 'Android Studio', 'XCode'],
+    },
+    more: {
+      summary: [
+        '기존 충전 서비스 프로젝트를 기반으로 Vue 3 도입, EV 충전, Wi-SUN 무선 통신으로 개선된 프로젝트로써, 기존 구조를 전체적으로 개선하며 서비스 확장을 경험한 프로젝트입니다.',
+      ],
+      config: [
+        '총 4종의 사용자 앱 / 매장 관리 웹 / 관리자 대시보드 웹으로 구성',
+        '웹 Vue 3 / 앱 Android(Java), iOS(Swift) Native 기반',
+        'IoT 전기차용 플러그와 Wi-SUN 무선 통신을 이용해 전력 제공 후 후불 결제',
+        '전기차 개방형 통신 규약(OCPP) 인증',
+        'Azure 클라우드 기반 Spring Boot 백엔드 서버',
+      ],
+      role: [
+        'Vue 3 + Vuetify 기반 웹 UI/UX 전반 개선 및 화면 구조 재설계',
+        '프론트엔드 · 앱(Android / iOS) 개발 담당으로 클라이언트 영역 전반 구현',
+        '클라이언트 – 서버 간 제어 · 결제 요청 흐름 연동 및 동작 검증',
+      ],
+      point: [
+        {
+          title: 'Vue 2 → Vue 3 마이그레이션 및 Composition API 기반 구조 전환',
+        },
+        {
+          title:
+            '웹 · 앱 전반 UI / UX 개선 및 사용자 시나리오 중심 화면 흐름 재정비',
+        },
+        {
+          title: 'BLE 직접 통신에서 Wi-SUN 무선 통신 기반 구조로 전환',
+        },
+        {
+          title: '선불 결제 방식에서 사용량 기반 후불 결제 흐름 설계 및 구현',
+        },
+        {
+          title:
+            'EV 충전 서비스 대응을 위한 OCPP 인증 연동 및 서비스 확장 경험',
+        },
+      ],
+      gallery: {
+        web: {
+          desktop: [
+            {
+              src: '/images/v_project/v_admin_dashboard/pc/v_admin_dashboard_pc_0.webp',
+              alt: '0',
+            },
+            {
+              src: '/images/v_project/v_admin_dashboard/pc/v_admin_dashboard_pc_1.webp',
+              alt: '1',
+            },
+            {
+              src: '/images/v_project/v_admin_dashboard/pc/v_admin_dashboard_pc_2.webp',
+              alt: '2',
+            },
+            {
+              src: '/images/v_project/v_admin_dashboard/pc/v_admin_dashboard_pc_3.webp',
+              alt: '3',
+            },
+            {
+              src: '/images/v_project/v_admin_dashboard/pc/v_admin_dashboard_pc_4.webp',
+              alt: '4',
+            },
+          ],
+          mobile: [
+            {
+              src: '/images/v_project/v_admin_dashboard/mobile/v_admin_dashboard_mobile_0.webp',
+              alt: '0',
+            },
+            {
+              src: '/images/v_project/v_admin_dashboard/mobile/v_admin_dashboard_mobile_1.webp',
+              alt: '1',
+            },
+            {
+              src: '/images/v_project/v_admin_dashboard/mobile/v_admin_dashboard_mobile_2.webp',
+              alt: '2',
+            },
+            {
+              src: '/images/v_project/v_admin_dashboard/mobile/v_admin_dashboard_mobile_3.webp',
+              alt: '3',
+            },
+            {
+              src: '/images/v_project/v_admin_dashboard/mobile/v_admin_dashboard_mobile_4.webp',
+              alt: '4',
+            },
+          ],
+        },
+        storeWeb: {
+          desktop: [
+            {
+              src: '/images/v_project/v_store_dashboard/pc/v_store_dashboard_pc_0.webp',
+              alt: '0',
+            },
+            {
+              src: '/images/v_project/v_store_dashboard/pc/v_store_dashboard_pc_1.webp',
+              alt: '1',
+            },
+            {
+              src: '/images/v_project/v_store_dashboard/pc/v_store_dashboard_pc_2.webp',
+              alt: '2',
+            },
+            {
+              src: '/images/v_project/v_store_dashboard/pc/v_store_dashboard_pc_3.webp',
+              alt: '3',
+            },
+            {
+              src: '/images/v_project/v_store_dashboard/pc/v_store_dashboard_pc_4.webp',
+              alt: '4',
+            },
+          ],
+          mobile: [
+            {
+              src: '/images/v_project/v_store_dashboard/mobile/v_store_dashboard_mobile_0.webp',
+              alt: '0',
+            },
+            {
+              src: '/images/v_project/v_store_dashboard/mobile/v_store_dashboard_mobile_1.webp',
+              alt: '1',
+            },
+            {
+              src: '/images/v_project/v_store_dashboard/mobile/v_store_dashboard_mobile_2.webp',
+              alt: '2',
+            },
+          ],
+        },
+        userApp: {
+          android: [
+            {
+              src: '/images/v_project/v_user_android/v_user_android_0.webp',
+              alt: '0',
+            },
+            {
+              src: '/images/v_project/v_user_android/v_user_android_1.webp',
+              alt: '1',
+            },
+            {
+              src: '/images/v_project/v_user_android/v_user_android_2.webp',
+              alt: '2',
+            },
+            {
+              src: '/images/v_project/v_user_android/v_user_android_3.webp',
+              alt: '3',
+            },
+            {
+              src: '/images/v_project/v_user_android/v_user_android_4.webp',
+              alt: '4',
+            },
+          ],
+          ios: [
+            {
+              src: '/images/v_project/v_user_ios/v_user_ios_0.webp',
+              alt: '0',
+            },
+            {
+              src: '/images/v_project/v_user_ios/v_user_ios_1.webp',
+              alt: '1',
+            },
+            {
+              src: '/images/v_project/v_user_ios/v_user_ios_2.webp',
+              alt: '2',
+            },
+            {
+              src: '/images/v_project/v_user_ios/v_user_ios_3.webp',
+              alt: '3',
+            },
+            {
+              src: '/images/v_project/v_user_ios/v_user_ios_4.webp',
+              alt: '4',
+            },
+          ],
+        },
+      },
     },
   },
   {
     index: 4,
     type: 2,
     status: 0,
-    title: '가스미터 검수 프로그램 개발',
+    title: '가스미터 검수 윈도우 프로그램 개발',
     content: 'TCP/IP 통신 프로토콜 기반 가스미터 기기 검수 프로그램 개발',
     period: { startDate: '2024-10', endDate: '2024-12' },
     task: ['Window Application'],
@@ -123,6 +410,44 @@ export const ProjectData: Project[] = [
       language: ['WPF(C#)'],
       dev: [''],
       tools: ['Visual Studio'],
+    },
+    more: {
+      summary: [
+        '수치를 검증하는 추가 기기와 TCP/IP 통신을 통해 장비 데이터를 수집·검증하는 Windows 클라이언트 프로그램',
+      ],
+      config: [
+        'Windows 클라이언트 (C#, WPF)',
+        '설정 파일 기반 환경 구성 및 로그 / 결과 저장 (txt)',
+        '리모컨 + 기기 연동(TCP/IP 통신)',
+      ],
+      role: [
+        'C# WPF 기반 Windows 클라이언트 개발',
+        '설정 파일(txt) 기반 환경 설정 및 관리 / 로그 저장',
+        '통신 포트 및 통신 속도 조절 설정 기능',
+        '리모컨 조절에 따른 실시간 데이터 수집',
+      ],
+      point: [
+        {
+          title:
+            'txt 설정 파일 기반으로 동적 조회 / 수정 / 삭제 가능한 구조 설계',
+        },
+        {
+          title: '설정 파일(txt) 기반 환경 설정 및 관리 / 로그 저장 기능 구현',
+        },
+        {
+          title: '기기와 TCP / IP 통신을 통한 실시간 데이터 수집 구현',
+        },
+      ],
+      gallery: {
+        windowApp: {
+          window: [
+            {
+              src: '/images/inspact_project/inspact_0.webp',
+              alt: '0',
+            },
+          ],
+        },
+      },
     },
   },
   {
@@ -141,6 +466,34 @@ export const ProjectData: Project[] = [
       dev: ['Docker', 'GitHub'],
       tools: ['VS Code', 'Android Studio'],
     },
+    more: {
+      summary: [
+        '현지화에 맞춘 IoT 플러그와 플랫폼으로 BLE/LTE 통신을 모두 제공하며, 실제 제품화에 초점을 둔 프로젝트',
+      ],
+      config: [
+        '총 4종의 사용자 앱 / 매장 관리 웹 / 관리자 대시보드 웹으로 구성',
+        '웹 Vue 3 / 앱 Android(Java), iOS(Swift) Native 기반',
+        '선불 결제 방식으로 사용자 통신망 환경에 따라 BLE/LTE로 기기제어',
+        'BLE를 통한 기기 LTE 정보 세팅',
+        'Azure 클라우드 기반 Spring Boot 백엔드 서버',
+      ],
+      role: [
+        'WEB 환경 모바일 블루투스 구현',
+        'BLE를 통한 LTE 정보 세팅 기능 구현',
+        '통신망 환경에 따라 BLE / LTE 통신 제어 변경',
+      ],
+      point: [
+        {
+          title: 'Web 환경에서 Android, iOS 블루투스 검증',
+        },
+        {
+          title: 'BLE를 통신을 이용한 LTE 설정값 세팅 검증',
+        },
+        {
+          title: 'LTE 통신 상태에 따라 BLE로 통신 요청하는 로직 검증',
+        },
+      ],
+    },
   },
   {
     index: 6,
@@ -156,6 +509,17 @@ export const ProjectData: Project[] = [
       language: ['Vue 3', 'TypeScript'],
       dev: ['Docker', 'GitHub'],
       tools: ['VS Code'],
+    },
+    more: {
+      summary: [''],
+      config: [''],
+      role: [''],
+      point: [
+        {
+          title: '',
+          content: [''],
+        },
+      ],
     },
   },
 ]
--- src/types/Project.ts
+++ src/types/Project.ts
@@ -1,4 +1,9 @@
 import type { Period } from './Common'
+interface Skill {
+  language: string[]
+  dev: string[]
+  tools: string[]
+}
 
 interface ProjectImage {
   src: string
@@ -7,6 +12,10 @@ interface ProjectImage {
 
 interface ProjectGallery {
   web?: {
+    desktop: ProjectImage[]
+    mobile?: ProjectImage[]
+  }
+  storeWeb?: {
     desktop: ProjectImage[]
     mobile?: ProjectImage[]
   }
@@ -22,12 +31,6 @@ interface ProjectGallery {
     window: ProjectImage[]
   }
 }
-interface Skill {
-  language: string[]
-  dev: string[]
-  tools: string[]
-}
-
 // type 1: Main / 2: Sube / 3: Personal
 // status 0: 완료, 1: 진행중, 2: (참여종료)
 export interface Project {
@@ -40,5 +43,15 @@ export interface Project {
   task: string[]
   personnel: number
   skills: Skill
-  gallery?: ProjectGallery
+  more: {
+    summary: string[] // 개요
+    config: string[] // 구성
+    role: string[] // 역할 & 주요 기여 내용
+    point: {
+      // 핵심 구현 포인트
+      title: string
+      content?: string[]
+    }[]
+    gallery?: ProjectGallery
+  }
 }
